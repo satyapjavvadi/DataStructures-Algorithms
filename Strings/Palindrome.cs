6404f7203ddf765c4b4a643ef8dad5ca6f279ded
Satya P Javvadi <saphin.rao@gmail.com>
2020-03-07 21:50:49 -0800

    Added Strings Project

---
+++ Strings/Palindrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings
+{
+    class Palindrome
+    {
+        static bool IsPalindrome(string str)
+        {
+            char[] ch = str.ToCharArray();
+            int first = 0, last = ch.Length - 1;
+
+            while (first < last)
+            {
+                if (ch[first] == ch[last])
+                {
+                    first++;
+                    last--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void Test()
+        {
+            //int[] arr = { 5, 4, 8, 9};
+            //int k = findsecondLargest(arr);
+            //Console.WriteLine(k);
+
+            string str = "madam";
+            bool b = IsPalindrome(str);
+            Console.WriteLine($"Palindrome is {b}");
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palindrome.Test();
+        }
+    }
+}
