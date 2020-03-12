e7773a3f64255bd18ee664e89324d1da4af14490
Satya P Javvadi <saphin.rao@gmail.com>
2020-03-11 23:55:15 -0700

    Added InsertionSort

---
+++ Arrays/InsertionSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class InsertionSort
+    { 
+        void sort(int[] a)
+        {
+            int n = a.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                int key = a[i];
+                int j = i - 1;
+
+
+                while (j >= 0 && a[j] > key)
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                }
+                a[j + 1] = key;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        public static void Test(String[] args)
+        {
+            int[] a = { 12, 11, 13, 5, 6 };
+            InsertionSort ob = new InsertionSort();
+            ob.sort(a);
+        }
+    }
+}
+
