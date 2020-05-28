f465694908e07e2f28914a0ed53414d4931df9a5
satyapjavvadi <saphin.rao@gmail.com>
2020-05-27 23:21:04 -0700

    Added CreateLinkedList program

---
+++ LinkedLists/CreateLinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLists
+{
+    public class Node
+    {
+        public int i;
+        public Node next;
+    }
+    public class Linked
+    {
+        public Node getNewNode(int key)
+        {
+            Node a = new Node();
+            a.i = key;
+            a.next = null;
+            return a;
+        }
+        public Node insert(int key, Node node)
+        {
+            if(node == null)
+            {
+                return getNewNode(key);
+            }
+            else
+            {
+                node.next = insert(key, node.next);
+            }
+            return node;
+        }
+    }
+    public class CreateLinkedList
+    {
+        public static void Test()
+        {
+            Linked a = new Linked();
+            Node root = null;
+            root = a.insert(12, root);
+            root = a.insert(99, root);
+        }
+    }
+}
+
+   
---
+++ LinkedLists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedLists
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateLinkedList.Test();
+        }
+    }
+}
