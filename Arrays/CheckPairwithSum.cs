4409264a3b83f238f3b0b371f291eda5887120c6
satyapjavvadi <saphin.rao@gmail.com>
2020-05-29 23:57:57 -0700

    Added CheckPairwithGivenSum program

---
+++ Arrays/CheckPairwithSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class CheckPairwithSum
+    {
+        public static bool IsPairExistsWithSum(int[] arr, int val)
+        {
+            for(int i = 0; i < arr.Length-1; i++)
+            {
+                for(int j = i+1; j < arr.Length; j++)
+                {
+                    if(arr[i] + arr[j] == val)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static void Test()
+        {
+            int[] arr = { 12, 3, 5, 1, 9 };
+            bool b = IsPairExistsWithSum(arr, 10);
+            Console.WriteLine(b);
+        }
+    }
+}
